e28395c6c378a510a9e58081a4c284b48ce6d253
Justin Cohen <Justin.H.Cohen@gmail.com>
2022-12-04 12:50:42 -0500

    Initial Commit

---
+++ Hearing Test Equalizer/Hearing_Test_EqualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hearing_Test_EqualizerApp.swift
+//  Hearing Test Equalizer
+//
+//  Created by Justin Cohen on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Hearing_Test_EqualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
